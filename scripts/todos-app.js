5bde5a0a200e99c5828fc64d5b4a00b4af3dabb5
Tanima Ranjan <tanimaranjan@gmail.com>
2019-05-31 23:10:29 -0400

    Todo app js

---
+++ scripts/todos-app.js
@@ -0,0 +1,34 @@
+'user strict'
+
+// Calling getSavedTodos on todos-fuction 
+const todos = getSavedTodos();
+
+const filters = {
+    searchText :'', 
+    hideCompleted : false
+}
+
+renderTodos(todos, filters);
+
+document.querySelector('#search-todo').addEventListener('input', (e) => {
+    filters.searchText = e.target.value;
+    renderTodos(todos, filters)
+})
+
+
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    e.preventDefault();
+    let newtodoEl = e.target.elements.newtodo.value;
+    if(newtodoEl.trim().length > 0) {
+        addNewTodo(newtodoEl)   
+    }
+    e.target.elements.newtodo.value = ''
+
+})
+
+document.querySelector('#hide-comp').addEventListener('change', (e) => {
+    filters.hideCompleted = e.target.checked;
+    renderTodos(todos, filters);
+})
+
+
